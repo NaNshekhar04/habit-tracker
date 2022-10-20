114b2105ea509d578de033caf14c5f817f729833
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-20 19:34:32 +0530

    Adding Habit Button and Styling it !

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <header className='container'>
+    <header className='header'>
         <h1>HABIT TRACKER</h1>
+        <button className='btn'>ADD HABIT</button>
     </header>
   )
 }
