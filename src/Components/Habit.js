aa87f2fed8829c6461979d3ba1849040c3d07f0c
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-20 20:39:40 +0530

    Installed react-Icons and used them !

--- src/Components/Habit.js
+++ src/Components/Habit.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import {FaTimes} from 'react-icons/fa'
+
 
 const Habit = ({habit}) => {
   return (
     <div className='habit'>
-        <h3>{habit.text}</h3>
+        <h3>{habit.text} <FaTimes style={{color:'red', cursor: 'pointer' }}/> </h3>
         <p>{habit.day}</p>
         </div>
   )
