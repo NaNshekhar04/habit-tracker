d3dc2e4271476f789f96f77dbb3022b7d232cf9d
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-11-09 19:11:51 +0530

    Adding Daily habit Status via checkBoxes !

--- src/Components/Habit.js
+++ src/Components/Habit.js
@@ -2,13 +2,26 @@ import { FaTimes } from 'react-icons/fa'
 
 
 const Habit = ({ habit, onDelete, onSwitch }) => {
+
   return (
 
-    // PARTICULAR HABITLIST PASSING DATA AND FUNCTIONS VIA PROPS 
+    // SINGLE HABIT COMPONENT !
 
     <div className='habitsList'>
       <div className={`habit ${habit.reminder ? 'reminder' : ''}`} onDoubleClick={() => onSwitch(habit.id)}>
-        <h3>{habit.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(habit.id)} /> </h3>
+        <h3>{habit.text} 
+          <ul className='dayStatus'>
+            <li><input type="checkbox" className='status'/></li>
+            <li><input type="checkbox" className='status'/></li>
+            <li><input type="checkbox" className='status' /></li>
+            <li><input type="checkbox" className='status'/></li>
+            <li><input type="checkbox" className='status'/></li>
+            <li><input type="checkbox" className='status'/></li>
+            <li><input type="checkbox" className='status'/></li>
+            
+          </ul>
+
+         <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(habit.id)} /> </h3>
         <p>{habit.day}</p>
       </div>
     </div>
